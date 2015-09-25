7942dc3a6352dcef7e84f80c42120145a03c58a7
ipandage <546657868@qq.com>
2015-09-25 18:21:34 +0800

    1

--- src/cn/org/rapid_framework/generator/GeneratorMain.java
+++ src/cn/org/rapid_framework/generator/GeneratorMain.java
@@ -3,8 +3,7 @@ package cn.org.rapid_framework.generator;
 
 /**
  * 
- * @author badqiu
- * @email badqiu(a)gmail.com
+ * @author gaoxingang
  */
 
 public class GeneratorMain {
@@ -16,7 +15,7 @@ public class GeneratorMain {
 //		g.printAllTableNames();				//打印数据库中的表名称
 		
 		g.deleteOutRootDir();							//删除生成器的输出目录
-		g.generateByTable("t_tracker","E:\\sourecode\\generator\\template");	//通过数据库表生成文件,template为模板的根目录
+		g.generateByTable("t_agent","E:\\git\\generator\\template");	//通过数据库表生成文件,template为模板的根目录
 //		g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
 //		g.generateByClass(Blog.class,"template_clazz");
 		 
